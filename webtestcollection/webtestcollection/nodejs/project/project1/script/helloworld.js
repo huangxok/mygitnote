1ff2279ad4cc6b4e106a981f8ffaeb70846a3441
huangxianwei <huangx.ok@163.com>
2015-08-17 18:21:07 +0800

    add file

--- webtestcollection/webtestcollection/nodejs/project/project1/script/helloworld.js
+++ webtestcollection/webtestcollection/nodejs/project/project1/script/helloworld.js
@@ -1,9 +1,10 @@
-﻿var http = require("http");
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
-    response.write("Hello，你妹!");
-    console.log(123);
-    response.end();
-}).listen(8888);
+﻿//var http = require("http");
+//http.createServer(function (request, response) {
+//    response.writeHead(200, {
+//        "Content-Type": "text/plain"
+//    });
+//    response.write("Hello，你妹!");
+//    console.log(123);
+//    response.end();
+//}).listen(8888);
+console.log('hello world');
