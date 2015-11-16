bcd1695b03f4320c6d633f7279f4f8701b474ddd
huangxianwei <huangx.ok@163.com>
2015-11-16 10:04:26 +0800

    update and  add files

--- webtestcollection/webtestcollection/requirejs/Script/main.js
+++ webtestcollection/webtestcollection/requirejs/Script/main.js
@@ -1,1 +1,12 @@
-﻿
+﻿require.config({
+  paths: {
+    "jquery": "jquery-1.7.1"
+  }
+});
+require(['jquery'], function ($) {
+  $('#kic').addClass('ak47');
+});
+require(['math'], function (math) {
+  var d = math.add(1, 2);
+  console.log(d);
+})
